8c34d0180bf78a86e179600efe7c5a8b7de0c17f
Bartosz Sobina <bartosz.sobina44@gmail.com>
2024-06-30 12:10:49 +0200

    configure fish shell using home manager

--- home.nix
+++ home.nix
@@ -10,6 +10,7 @@
 			tree
       qbittorrent
       python313
+      yt-dlp
 		];
 	};
 
@@ -19,6 +20,7 @@
     userEmail = "bartosz.sobina44@gmail.com";
 
     aliases = {
+      s = "status";
       d = "diff";
       co = "checkout";
       ci = "commit";
@@ -27,12 +29,44 @@
       pl = "pull";
       br = "branch";
     };
+  };
 
-    extraConfig = {
-      push = {
-        default = "simple";
+  programs.fish = {
+    enable = true;
+    interactiveShellInit = ''
+          set fish_greeting # Disable greeting
+    '';
+    shellAbbrs = {
+      c = "clear";
+      n = "nvim";
+      ls = "eza --icons";
+      l = "eza --icons ";
+      ll = "eza --icons -ahl";
+      lt = "eza --tree";
+      cd = "z";
+      g = "git";
+    };
+
+    functions = {
+      fish_prompt = {
+        body = ''
+          set_color red --bold
+          printf "["
+          set_color yellow
+          printf "%s" "$USER"
+          set_color green
+          printf "@"
+          set_color blue
+          printf "%s" "$hostname"
+          set_color magenta
+          printf " %s" "$PWD"
+          set_color red
+          printf "] "
+          set_color normal
+        '';
       };
     };
+    shellInitLast = "zoxide init fish | source | fish_config theme choose Dracula";
   };
 
 	home.pointerCursor = {
