c872a81b6a02168b51af2695a603191b43f753da
Ressobe <bartosz.sobina44@gmail.com>
2024-06-30 09:42:20 +0200

    configure git using home manager

--- home.nix
+++ home.nix
@@ -13,6 +13,28 @@
 		];
 	};
 
+  programs.git = {
+    enable = true;
+    userName = "Ressobe";
+    userEmail = "bartosz.sobina44@gmail.com";
+
+    aliases = {
+      d = "diff";
+      co = "checkout";
+      ci = "commit";
+      ca = "commit -a";
+      ps = "push";
+      pl = "pull";
+      br = "branch";
+    };
+
+    extraConfig = {
+      push = {
+        default = "simple";
+      };
+    };
+  };
+
 	home.pointerCursor = {
 		package = pkgs.vanilla-dmz;
       		name = "Vanilla-DMZ";
